debad46bbe30b244ba866e3bdd3d142a0996df24
kastastin <kostya242421@gmail.com>
2024-08-12 00:19:51 +0300

    add root layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const Home = () => {
   return (
     <div className="flex gap-4">
       <h1 className="h1-bold">Dev Overflow</h1>
-
-      <UserButton />
     </div>
   );
 };
