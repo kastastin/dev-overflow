8e9b71bbc4e753c5f1fac6b3962b26d51eff4b76
kastastin <kostya242421@gmail.com>
2024-08-28 00:49:02 +0300

    add no-result shered component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,38 @@ import Link from "next/link";
 import Filter from "@/components/shared/Filter";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import NoResult from "@/components/shared/NoResult";
 import HomeFilters from "@/components/home/HomeFilters";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: "Best practices for data fetching",
+  //   tags: [
+  //     { _id: 1, name: "next" },
+  //     { _id: 2, name: "javascript" },
+  //   ],
+  //   author: "John Snow",
+  //   upvotes: 10,
+  //   views: 121,
+  //   answers: 2,
+  //   createdAt: "2024-08-05T11:14:23.429Z",
+  // },
+  // {
+  //   _id: 2,
+  //   title: "How to Perfectly Center a Div with Tailwind CSS?",
+  //   tags: [
+  //     { _id: 1, name: "css" },
+  //     { _id: 2, name: "javascript" },
+  //   ],
+  //   author: "John Snow",
+  //   upvotes: 4,
+  //   views: 36,
+  //   answers: 1,
+  //   createdAt: "2024-07-03T10:22:11.153Z",
+  // },
+];
 
 const Home = () => {
   return (
@@ -37,6 +67,19 @@ const Home = () => {
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => "QuestionCard")
+        ) : (
+          <NoResult
+            title="There's no questions to show"
+              description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved! 💡"
+              link="/ask-question"
+              linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
