5173a2b39b23ba8650a167eedc6a0fb0c6c2a4af
kastastin <kostya242421@gmail.com>
2024-09-13 13:16:35 +0300

    create global search Result component

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -1,5 +1,118 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useSearchParams } from "next/navigation";
+
+import GlobalFilters from "@/components/shared/search/GlobalFilters";
+
 const GlobalResult = () => {
-  return <div>GlobalResult</div>;
+  const searchParams = useSearchParams();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([
+    { type: "question", id: 1, title: "Next.js question" },
+    { type: "tag", id: 1, title: "Nextjs" },
+    { type: "user", id: 1, title: "jsm" },
+  ]);
+
+  const type = searchParams.get("type");
+  const global = searchParams.get("global");
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      setIsLoading(true);
+      setData([]);
+
+      try {
+        // const result = await globalSearch({ query: global, type });
+        // setData(JSON.parse(result));
+      } catch (error) {
+        console.error(error);
+        throw error;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (global) fetchResult();
+  }, [type, global]);
+
+  const renderLink = (id: string, type: string) => {
+    switch (type) {
+      case "question":
+        return `/question/${id}`;
+      case "answer":
+        return `/question/${id}`;
+      case "user":
+        return `/profile/${id}`;
+      case "tag":
+        return `/tags/${id}`;
+      default:
+        return "/";
+    }
+  };
+
+  return (
+    <div className="absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
+      <GlobalFilters />
+
+      <div className="my-5 h-px bg-light-700/50 dark:bg-dark-500/50" />
+      <div className="space-y-5">
+        <p className="text-dark400_light900 paragraph-semibold px-5">
+          Top Match
+        </p>
+
+        {isLoading ? (
+          <div className="flex-center flex-col px-5">
+            <ReloadIcon className="my-2 size-10 animate-spin text-primary-500" />
+
+            <p className="text-dark200_light800 body-regular">
+              Browsing the entire database
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {data.length > 0 ? (
+              data.map((item: any, index: number) => (
+                <Link
+                  key={item.type + item.id + index}
+                  href={renderLink(item.id, item.type)}
+                  className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-500/50"
+                >
+                  <Image
+                    src="/assets/icons/tag.svg"
+                    alt="tags"
+                    width={18}
+                    height={18}
+                    className="invert-colors mt-1 object-contain"
+                  />
+
+                  <div className="flex flex-col">
+                    <p className="body-medium text-dark200_light800 line-clamp-1">
+                      {item.title}
+                    </p>
+
+                    <p className="text-light400_light500 small-medium mt-1 font-bold capitalize">
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="flex-center flex-col px-5">
+                <p className="text-dark200_light800 body-regular px-5 py-2.5">
+                  Oops, no results found
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default GlobalResult;
